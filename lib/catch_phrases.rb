8d6781c31b0eed37b1eb17428a05dbee1f8da67a
Adrian Samaniego <llathander@gmail.com>
2020-01-20 22:23:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,31 +1,29 @@
+
+
+
+
+
+
+
 def mario
-  status = ""
-
-
-
-
-
-
-
-#def mario
-#<<<<<<< HEAD
-#  phrase = "It's-a me, Mario!"
+<<<<<<< HEAD
+ phrase = "It's-a me, Mario!"
 #=======
-#  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
-#>>>>>>> 39c7e11b62a92bd3b48d8c84f9d13f5a08cad283
-#  puts phrase
-#end
+  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
+>>>>>>> 39c7e11b62a92bd3b48d8c84f9d13f5a08cad283
+  puts phrase
+end
 
-#def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-#  puts status
-#end
+def toadstool
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ puts status
+end
 
-#def link 
- # phrase = "It's Dangerous To Go Alone! Take This."
-#  puts phrase
-#end
+def link 
+ phrase = "It's Dangerous To Go Alone! Take This."
+ puts phrase
+end
 
-#def any_phrase(phrase)
-#  puts phrase
-#end
+def any_phrase(phrase)
+ puts phrase
+end
