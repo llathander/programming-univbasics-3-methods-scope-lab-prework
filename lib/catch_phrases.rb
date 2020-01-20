b68011939236dc2168b973fcc6fc455ce3e73ef8
Adrian Samaniego <llathander@gmail.com>
2020-01-20 22:23:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,11 +6,8 @@
 
 
 def mario
-<<<<<<< HEAD
- phrase = "It's-a me, Mario!"
-#=======
+phrase = "It's-a me, Mario!"
   phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
->>>>>>> 39c7e11b62a92bd3b48d8c84f9d13f5a08cad283
   puts phrase
 end
 
